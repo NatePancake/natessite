a28e9447afa2039250ebc91c672f4bc0417ec980
NatePancake <therealseko4@gmail.com>
2026-05-31 16:56:11 +0800

    michael and jackson are looking down on me

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-const API_KEY = "AIzaSyCFtlUTd3FJ9PPPo9IxebNc3lJqd1B_a-0"
-const CHANNEL_ID = "UCKnMLdK7r4tlbH38heG8lUg"
+// const API_KEY = "AIzaSyCFtlUTd3FJ9PPPo9IxebNc3lJqd1B_a-0"
+// const CHANNEL_ID = "UCKnMLdK7r4tlbH38heG8lUg"
 
 // Construct the API URL
-const url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${CHANNEL_ID}&key=${API_KEY}`;
+// const url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${CHANNEL_ID}&key=${API_KEY}`;
 
 // Fetch subscriber data
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    const subCount = data.items[0].statistics.subscriberCount;
-    document.getElementById("subcount").textContent = Number(subCount).toLocaleString()
-    console.log(subCount)
-  })
-  .catch(error => {
-    console.error("Error fetching subscriber data:", error);
-    document.getElementById("subcount").textContent = "Failed to load";
-  });
+// fetch(url)
+  // .then(response => response.json())
+  // .then(data => {
+    // const subCount = data.items[0].statistics.subscriberCount;
+    // document.getElementById("subcount").textContent = Number(subCount).toLocaleString()
+    // console.log(subCount)
+  // })
+  // .catch(error => {
+    // console.error("Error fetching subscriber data:", error);
+    // document.getElementById("subcount").textContent = "Failed to load";
+  // });
 
   // above stuff was made by chatgpt, i am shameful of this, as well as part of the stuff
   // below this
